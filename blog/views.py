d513f4114b6acc34653d9e0bc694e34f00daa544
arctenis <arctenischloria@gmail.com>
2022-11-19 14:47:17 +0100

    FEAT Display similar posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
@@ -18,9 +19,23 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
+    # Active comments list
     comments = post.comments.filter(active=True)
+    # Form to add a comment
     form = CommentForm()
-    context = {"post": post, "comments": comments, "form": form}
+    # Similar posts
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "publish"
+    )[:4]
+
+    context = {
+        "post": post,
+        "comments": comments,
+        "form": form,
+        "similar_posts": similar_posts,
+    }
     return render(request, "blog/post/detail.html", context)
 
 
