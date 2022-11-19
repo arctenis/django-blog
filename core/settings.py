abcc13a78a564d38356f1d61f771f79e9b03d319
arctenis <arctenischloria@gmail.com>
2022-11-19 21:13:15 +0100

    FEAT Add sitemap.xml

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Additions
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
     # Local apps
     "blog.apps.BlogConfig",
     # 3rd party
